87bd4a492e94d142579d7cfa5ac42e5d179f5808
SinnerSS <ntnbusinf@gmail.com>
2023-05-29 00:43:31 +0700

    basic game window implementation
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

---
+++ src/main/java/com/group/platformgame/core/GameWindow.java
@@ -0,0 +1,11 @@
+package main.java.com.group.platformgame.core;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame{
+    public GameWindow() {
+        setSize(1280,1024);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
