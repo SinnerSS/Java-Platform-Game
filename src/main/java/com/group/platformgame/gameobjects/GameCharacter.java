f1e195ff34fb09cebf464a4945fbb72e21adb5c4
SinnerSS <ntnbusinf@gmail.com>
2023-05-29 08:06:18 +0700

    remove prototype field

--- src/main/java/com/group/platformgame/gameobjects/GameCharacter.java
+++ src/main/java/com/group/platformgame/gameobjects/GameCharacter.java
@@ -5,20 +5,17 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class GameCharacter extends GameObject {
-  private int i = 0;
   private BufferedImage animationBlock = null;
 
   public GameCharacter(int x, int y, File spriteFile) {
     super(x, y, spriteFile);
   }
 
-  public void update(Graphics g) {
-    //TODO: iterate through charactersheet
-    animationBlock = sprite.getSubimage(0, 0, 128, 64);
-    render(g);
+  public void update() {
   }
 
   public void render(Graphics g){
+    animationBlock = sprite.getSubimage(0, 0, 128, 64);
     g.drawImage(animationBlock, position.get(0), position.get(1), null);
   } 
 }
