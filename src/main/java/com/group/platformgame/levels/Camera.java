20800f493574b84f015f747453b562dd76e08d59
SinnerSS <ntnbusinf@gmail.com>
2023-06-09 01:29:17 +0700

    add transformation method
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/levels/Camera.java
+++ src/main/java/com/group/platformgame/levels/Camera.java
@@ -1,5 +1,6 @@
 package main.java.com.group.platformgame.levels;
 
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -9,6 +10,21 @@ public class Camera implements KeyListener {
     public Camera(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Point applyTransformation(int x, int y) {
+        int translatedX = x - this.x;
+        int translatedY = y - this.y;
+
+        return new Point(translatedX, translatedY);
     }
     public int xRelative(int x) {
         return x - this.x;
@@ -22,6 +38,8 @@ public class Camera implements KeyListener {
         if(e.getKeyChar() == 'd') x += 10;
         if(e.getKeyChar() == 'a') x -= 10;
     }
-    public void keyTyped(KeyEvent e) {}
-    public void keyReleased(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
+    public void keyReleased(KeyEvent e) {
+    }
 }
