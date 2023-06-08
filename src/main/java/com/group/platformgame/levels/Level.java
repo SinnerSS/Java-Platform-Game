7231594e5061a3311bbe22f8a028a094940a4e9a
SinnerSS <ntnbusinf@gmail.com>
2023-06-09 01:30:11 +0700

    add render method
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/levels/Level.java
+++ src/main/java/com/group/platformgame/levels/Level.java
@@ -7,7 +7,10 @@ import java.util.List;
 import java.awt.Graphics2D;
 
 public class Level {
+  public static int CELL_HEIGHT = 16;
+  public static int CELL_WIDTH = 16;
 
+  private List<TextureMap> mapMem = new ArrayList<>();
   private int[][] gridData;
   private Camera camera;
   public Level(String levelFile) {
@@ -19,6 +22,40 @@ public class Level {
   public Camera getCamera() {
     return camera;
   }
+
+  public void render(Graphics2D g2d) {
+    int numRows = gridData.length;
+    int numCols = gridData[0].length;
+
+    int startX = Math.max(0, camera.getX() / CELL_WIDTH); 
+    int startY = Math.max(0, camera.getY() / CELL_HEIGHT); 
+    
+    int endX = Math.min(numRows, (camera.getX() + 1280) / CELL_WIDTH + 1); 
+    int endY = Math.min(numCols, (camera.getY() + 1024) / CELL_HEIGHT + 1); 
+    
+    for (int row = startX; row < endX; row++) {
+      for (int col = startY; col < endY; col++) {
+        int textureKey = gridData[row][col];
+        BufferedImage textureImage = null;
+
+        for(TextureMap type : TextureMap.values()) {
+          if(type.inRange(textureKey)) {
+            if(!mapMem.contains(type)) mapMem.add(type);
+            textureImage = type.loadTexture(textureKey);
+          }
+        }
+      
+      
+
+        int xPos = col * CELL_WIDTH; 
+        int yPos = row * CELL_HEIGHT; 
+          
+
+        Point transformedPos = camera.applyTransformation(xPos, yPos);
+          
+        
+        g2d.drawImage(textureImage, transformedPos.x, transformedPos.y, null);
+      }
+    }
+  }
 }
-
-
