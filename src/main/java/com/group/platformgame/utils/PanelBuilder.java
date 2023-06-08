8323fba28fa769da18363309dc7a7f293bf0e2ad
SinnerSS <ntnbusinf@gmail.com>
2023-06-09 01:30:59 +0700

    fix spacing
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/utils/PanelBuilder.java
+++ src/main/java/com/group/platformgame/utils/PanelBuilder.java
@@ -5,11 +5,11 @@ import java.awt.GridBagConstraints;
 
 public class PanelBuilder {
 
-    public static GridBagConstraints changeCords (GridBagConstraints layout, int x, int y) {
-        layout.gridx = x;
-        layout.gridy = y;
+  public static GridBagConstraints changeCords (GridBagConstraints layout, int x, int y) {
+    layout.gridx = x;
+    layout.gridy = y;
 
-        return layout;
-    }
+    return layout;
+  }
     
 }
