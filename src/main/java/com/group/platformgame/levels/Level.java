e80202f3a12d437199093dfa130084be2dc7d8c3
SinnerSS <ntnbusinf@gmail.com>
2023-06-09 01:28:15 +0700

    refactor to remove custom level grid

--- src/main/java/com/group/platformgame/levels/Level.java
+++ src/main/java/com/group/platformgame/levels/Level.java
@@ -1,22 +1,24 @@
 package main.java.com.group.platformgame.levels;
 
-import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.Graphics2D;
-import javax.swing.JPanel;
 
-public class Level extends JPanel{
-    private LevelGrid levelGrid;
-    private Camera camera;
-    public Level(String levelFile) {
-        LevelParser parser = new LevelParser(levelFile);
-        levelGrid = parser.getLevelGrid();
-        camera = parser.getCamera();
-        addKeyListener(camera);
-    }
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2D = (Graphics2D) g;
+public class Level {
 
-        levelGrid.render(g2D, camera);
-    }
+  private int[][] gridData;
+  private Camera camera;
+  public Level(String levelFile) {
+    LevelParser parser = new LevelParser(levelFile);
+    gridData = parser.getLevelGrid();
+    camera = parser.getCamera();
+  }
+  
+  public Camera getCamera() {
+    return camera;
+  }
 }
+
+
