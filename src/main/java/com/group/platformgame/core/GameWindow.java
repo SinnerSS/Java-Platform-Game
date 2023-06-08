5ffc753300b5e304e8bab09591f5d6c625d8b772
SinnerSS <ntnbusinf@gmail.com>
2023-06-09 01:30:59 +0700

    fix spacing
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/core/GameWindow.java
+++ src/main/java/com/group/platformgame/core/GameWindow.java
@@ -3,9 +3,9 @@ package main.java.com.group.platformgame.core;
 import javax.swing.JFrame;
 
 public class GameWindow extends JFrame{
-    public GameWindow() {
-        setSize(1280,1024);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+  public GameWindow() {
+    setSize(1280,1024);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
 }
