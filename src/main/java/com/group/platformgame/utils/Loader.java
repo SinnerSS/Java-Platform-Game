4aed06053db378965fde3f8c264b8b4c94274e74
SinnerSS <ntnbusinf@gmail.com>
2023-06-12 00:14:58 +0700

    fix wrong class name
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/utils/Loader.java
+++ src/main/java/com/group/platformgame/utils/Loader.java
@@ -5,12 +5,10 @@ import java.io.InputStream;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
-import main.java.com.group.platformgame.levels.TextureMap;
-
 public class Loader {
   public static BufferedImage loadBufferedImage(String path) {
     BufferedImage image = null;
-    InputStream is = TextureMap.class.getResourceAsStream(path);
+    InputStream is = Loader.class.getResourceAsStream(path);
     try {
       image = ImageIO.read(is);
       is.close();
