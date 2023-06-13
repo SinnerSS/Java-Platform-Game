bed7aefa3de864acae70b57f7525991b990e62eb
SinnerSS <ntnbusinf@gmail.com>
2023-06-13 19:38:32 +0700

    add zoom
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/levels/Camera.java
+++ src/main/java/com/group/platformgame/levels/Camera.java
@@ -3,15 +3,20 @@ package main.java.com.group.platformgame.levels;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Camera implements KeyListener {
+public class Camera implements KeyListener, MouseListener {
     private int x;
     private int y;
+    private float zoom = 2;
     public Camera(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    
+    public float getZoom() {
+        return zoom;
+    }
     public int getX() {
         return x;
     }
@@ -33,13 +38,29 @@ public class Camera implements KeyListener {
         return y - this.y;
     }
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyChar() == 'w') y -= 10;
-        if(e.getKeyChar() == 's') y += 10;
-        if(e.getKeyChar() == 'd') x += 10;
-        if(e.getKeyChar() == 'a') x -= 10;
+        if(e.getKeyChar() == 'w') y -= 32;
+        if(e.getKeyChar() == 's') y += 32;
+        if(e.getKeyChar() == 'd') x += 32;
+        if(e.getKeyChar() == 'a') x -= 32;
     }
     public void keyTyped(KeyEvent e) {
     }
     public void keyReleased(KeyEvent e) {
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println(e.getX() + " " + e.getY());
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
