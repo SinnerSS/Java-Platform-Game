61afef68e6c5227189d4a45b24b1a767aa0f38b3
SinnerSS <ntnbusinf@gmail.com>
2023-06-13 19:38:32 +0700

    add zoom
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/levels/Level.java
+++ src/main/java/com/group/platformgame/levels/Level.java
@@ -1,9 +1,14 @@
 package main.java.com.group.platformgame.levels;
 
 import java.awt.Point;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+
+import main.java.com.group.platformgame.core.GamePanel;
+
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class Level {
@@ -23,39 +28,48 @@ public class Level {
     return camera;
   }
 
-  public void render(Graphics2D g2d) {
-    int numRows = gridData.length;
-    int numCols = gridData[0].length;
+public void render(Graphics2D g2d) {
+  g2d.setColor(Color.WHITE);
+  g2d.fillRect(0, 0, 1280, 1024);
 
-    int startX = Math.max(0, camera.getX() / CELL_WIDTH); 
-    int startY = Math.max(0, camera.getY() / CELL_HEIGHT); 
-    
-    int endX = Math.min(numRows, (camera.getX() + 1280) / CELL_WIDTH + 1); 
-    int endY = Math.min(numCols, (camera.getY() + 1024) / CELL_HEIGHT + 1); 
-    
-    for (int row = startX; row < endX; row++) {
-      for (int col = startY; col < endY; col++) {
-        int textureKey = gridData[row][col];
-        BufferedImage textureImage = null;
+  double zoomFactor = camera.getZoom();
+  int numRows = gridData.length - 1;
+  int numCols = gridData[0].length - 1;
 
-        for(TextureMap type : TextureMap.values()) {
-          if(type.inRange(textureKey)) {
-            if(!mapMem.contains(type)) mapMem.add(type);
-            textureImage = type.loadTexture(textureKey);
-          }
+  int visibleWidth = GamePanel.WIDTH;
+  int visibleHeight = GamePanel.HEIGHT;
+
+  int startX = Math.max(0, (camera.getX() - visibleWidth / 2) / CELL_WIDTH);
+  int startY = Math.max(0, (camera.getY() - visibleHeight / 2) / CELL_HEIGHT);
+
+  int endX = Math.min(numCols, (camera.getX() + visibleWidth / 2) / CELL_WIDTH + 1);
+  int endY = Math.min(numRows, (camera.getY() + visibleHeight / 2) / CELL_HEIGHT + 1);
+
+  AffineTransform originalTransform = g2d.getTransform();
+
+  g2d.scale(zoomFactor, zoomFactor);
+
+  for (int row = startY; row < endY; row++) {
+    for (int col = startX; col < endX; col++) {
+      int textureKey = gridData[row][col];
+      BufferedImage textureImage = null;
+
+      for (TextureMap type : TextureMap.values()) {
+        if (type.inRange(textureKey)) {
+          if (!mapMem.contains(type)) mapMem.add(type);
+          textureImage = type.loadTexture(textureKey);
         }
-      
-      
-
-        int xPos = col * CELL_WIDTH; 
-        int yPos = row * CELL_HEIGHT; 
-          
-
-        Point transformedPos = camera.applyTransformation(xPos, yPos);
-          
-        
-        g2d.drawImage(textureImage, transformedPos.x, transformedPos.y, null);
       }
+
+      int xPos = col * CELL_WIDTH;
+      int yPos = row * CELL_HEIGHT;
+
+      Point transformedPos = camera.applyTransformation(xPos, yPos);
+
+      g2d.drawImage(textureImage, transformedPos.x, transformedPos.y, null);
     }
   }
+
+  g2d.setTransform(originalTransform);
+}
 }
