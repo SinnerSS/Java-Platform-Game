f7eb32aa1e168b5d9edeaeb850a793e498714073
SinnerSS <ntnbusinf@gmail.com>
2023-06-15 00:50:01 +0700

    add player field

--- src/main/java/com/group/platformgame/levels/Level.java
+++ src/main/java/com/group/platformgame/levels/Level.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import main.java.com.group.platformgame.core.GamePanel;
+import main.java.com.group.platformgame.gameobjects.character.Player;
 import main.java.com.group.platformgame.utils.Loader;
 
 import java.awt.Color;
@@ -20,10 +21,12 @@ public class Level {
   private int[][] gridData;
   private Camera camera;
   private BufferedImage background;
+  private Player player;
   public Level(String levelFile) {
     LevelParser parser = new LevelParser(levelFile);
     gridData = parser.getLevelGrid();
     camera = parser.getCamera();
+    player = parser.getPlayer();
     background = Loader.loadBufferedImage("/resources/assets/images/platforms/Background.png");
   }
   
@@ -35,50 +38,57 @@ public class Level {
     return camera;
   }
 
-public void render(Graphics2D g2d) {
-  g2d.setColor(Color.WHITE);
-  g2d.fillRect(0, 0, 1280, 1024);
+  public void render(Graphics2D g2d) {
+    g2d.setColor(Color.WHITE);
+    g2d.fillRect(0, 0, 1280, 1024);
 
-  g2d.drawImage(background, 0, 0, GamePanel.WIDTH, GamePanel.HEIGHT, null);
+    g2d.drawImage(background, 0, 0, GamePanel.WIDTH, GamePanel.HEIGHT, null);
 
-  double zoomFactor = camera.getZoom();
-  int numRows = gridData.length - 1;
-  int numCols = gridData[0].length - 1;
+    double zoomFactor = camera.getZoom();
 
-  int visibleWidth = GamePanel.WIDTH;
-  int visibleHeight = GamePanel.HEIGHT;
+    AffineTransform originalTransform = g2d.getTransform();
 
-  int startX = Math.max(0, (camera.getX() - visibleWidth / 2) / CELL_WIDTH);
-  int startY = Math.max(0, (camera.getY() - visibleHeight / 2) / CELL_HEIGHT);
+    g2d.scale(zoomFactor, zoomFactor);
 
-  int endX = Math.min(numCols, (camera.getX() + visibleWidth / 2) / CELL_WIDTH + 1);
-  int endY = Math.min(numRows, (camera.getY() + visibleHeight / 2) / CELL_HEIGHT + 1);
+    int numRows = gridData.length - 1;
+    int numCols = gridData[0].length - 1;
 
-  AffineTransform originalTransform = g2d.getTransform();
+    int visibleWidth = GamePanel.WIDTH;
+    int visibleHeight = GamePanel.HEIGHT;
 
-  g2d.scale(zoomFactor, zoomFactor);
+    int startX = Math.max(0, (camera.getX() - visibleWidth / 2) / CELL_WIDTH);
+    int startY = Math.max(0, (camera.getY() - visibleHeight / 2) / CELL_HEIGHT);
 
-  for (int row = startY; row < endY; row++) {
-    for (int col = startX; col < endX; col++) {
-      int textureKey = gridData[row][col];
-      BufferedImage textureImage = null;
+    int endX = Math.min(numCols, (camera.getX() + visibleWidth / 2) / CELL_WIDTH + 1);
+    int endY = Math.min(numRows, (camera.getY() + visibleHeight / 2) / CELL_HEIGHT + 1);
 
-      for (TextureMap type : TextureMap.values()) {
-        if (type.inRange(textureKey)) {
-          if (!mapMem.contains(type)) mapMem.add(type);
-          textureImage = type.loadTexture(textureKey);
+    for (int row = startY; row < endY; row++) {
+      for (int col = startX; col < endX; col++) {
+        int textureKey = gridData[row][col];
+        BufferedImage textureImage = null;
+
+        for (TextureMap type : TextureMap.values()) {
+          if (type.inRange(textureKey)) {
+            if (!mapMem.contains(type)) mapMem.add(type);
+            textureImage = type.loadTexture(textureKey);
+          }
         }
+
+        int xPos = col * CELL_WIDTH;
+        int yPos = row * CELL_HEIGHT;
+
+        Point transformedPos = camera.applyTransformation(xPos, yPos);
+
+        g2d.drawImage(textureImage, transformedPos.x, transformedPos.y, null);
       }
-
-      int xPos = col * CELL_WIDTH;
-      int yPos = row * CELL_HEIGHT;
-
-      Point transformedPos = camera.applyTransformation(xPos, yPos);
-
-      g2d.drawImage(textureImage, transformedPos.x, transformedPos.y, null);
     }
+    
+    player.render(g2d);
+
+    g2d.setTransform(originalTransform);
   }
 
-  g2d.setTransform(originalTransform);
-}
+  public Player getPlayer() {
+    return player;
+  }
 }
