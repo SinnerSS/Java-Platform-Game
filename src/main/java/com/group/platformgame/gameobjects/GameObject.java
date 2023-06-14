3d94bd68f36a3fb52f61d95e956a89586a35a86b
SinnerSS <ntnbusinf@gmail.com>
2023-06-15 00:48:17 +0700

    refactor position and remove sprite
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/gameobjects/GameObject.java
+++ src/main/java/com/group/platformgame/gameobjects/GameObject.java
@@ -1,34 +1,37 @@
 package main.java.com.group.platformgame.gameobjects;
 
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.util.List;
-import java.util.ArrayList;
-import java.io.IOException;
-import java.io.File;
-import javax.imageio.ImageIO;
+
 
 public abstract class GameObject {
-  protected List<Integer> position = new ArrayList<>();
-  protected BufferedImage sprite = null;
+  protected int x;
+  protected int y;
 
-  public GameObject(int x, int y, File spriteFile) {
-    position.add(x);
-    position.add(y);
-    try {
-      sprite = ImageIO.read(spriteFile);
-    } catch(IOException e) {
-      System.out.println("Cant read sprite file!");
-    }
+  public GameObject(int x, int y) {
+    this.x = x;
+    this.y = y;
   }
+
+  
+  public int getX() {
+    return x;
+  }
+
 
   public void setX(int x) {
-    position.set(0, x);
+    this.x = x;
   }
 
-  public void setY(int y) {
-    position.set(1, y);
+
+  public int getY() {
+    return y;
   }
+
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
 
   public abstract void update(); 
 
