90be9a6d003049c803ef64cd057a016b96f9d86b
SinnerSS <ntnbusinf@gmail.com>
2023-06-15 00:48:44 +0700

    refactor to use new game object

--- src/main/java/com/group/platformgame/gameobjects/character/GameCharacter.java
+++ src/main/java/com/group/platformgame/gameobjects/character/GameCharacter.java
@@ -2,22 +2,21 @@ package main.java.com.group.platformgame.gameobjects.character;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
 
 import main.java.com.group.platformgame.gameobjects.GameObject;
 
 public class GameCharacter extends GameObject {
-  private BufferedImage animationBlock = null;
+  protected BufferedImage spriteSheet;
+  protected int animationTick = 0;
 
-  public GameCharacter(int x, int y, File spriteFile) {
-    super(x, y, spriteFile);
+  public GameCharacter(int x, int y) {
+    super(x, y);
   }
 
   public void update() {
   }
 
   public void render(Graphics g){
-    animationBlock = sprite.getSubimage(0, 0, 128, 64);
-    g.drawImage(animationBlock, position.get(0), position.get(1), null);
+
   } 
 }
