e19d7aece97df2fcad25393ead315e584ca6d584
SinnerSS <ntnbusinf@gmail.com>
2023-06-16 23:31:18 +0700

    add update method
    Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- src/main/java/com/group/platformgame/levels/Camera.java
+++ src/main/java/com/group/platformgame/levels/Camera.java
@@ -2,10 +2,13 @@ package main.java.com.group.platformgame.levels;
 
 import java.awt.Graphics2D;
 
+import main.java.com.group.platformgame.core.GamePanel;
+import main.java.com.group.platformgame.gameobjects.character.Player;
+
 public class Camera {
     private int x;
     private int y;
-    private float zoom = 2;
+    private float zoom = 3;
     public Camera(int x, int y) {
         this.x = x;
         this.y = y;
@@ -19,6 +22,11 @@ public class Camera {
 
     public int getY() {
         return y;
+    }
+
+    public void update(Player player) {
+        x = player.getX() - GamePanel.WIDTH / 2;
+        y = player.getY() - GamePanel.HEIGHT / 2;
     }
 
     public Graphics2D applyTransformation(Graphics2D g2D) {
