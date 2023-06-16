e0634209efd67d3aba307602db1e89a613353b86
SinnerSS <ntnbusinf@gmail.com>
2023-06-16 23:25:00 +0700

    remove for testing function

--- src/main/java/com/group/platformgame/levels/Camera.java
+++ src/main/java/com/group/platformgame/levels/Camera.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class Camera implements KeyListener, MouseListener {
+public class Camera {
     private int x;
     private int y;
     private float zoom = 2;
@@ -30,37 +30,5 @@ public class Camera implements KeyListener, MouseListener {
         int translatedY = y - this.y;
 
         return new Point(translatedX, translatedY);
-    }
-    public int xRelative(int x) {
-        return x - this.x;
-    }
-    public int yRelative(int y) {
-        return y - this.y;
-    }
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyChar() == 'w') y -= 32;
-        if(e.getKeyChar() == 's') y += 32;
-        if(e.getKeyChar() == 'd') x += 32;
-        if(e.getKeyChar() == 'a') x -= 32;
-    }
-    public void keyTyped(KeyEvent e) {
-    }
-    public void keyReleased(KeyEvent e) {
-    }
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println(e.getX() + " " + e.getY());
-    }
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-    @Override
-    public void mouseReleased(MouseEvent e) {
-    }
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-    @Override
-    public void mouseExited(MouseEvent e) {
     }
 }
