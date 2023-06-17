b5b32753532cf1eda0e24b531210999ca9352181
SinnerSS <ntnbusinf@gmail.com>
2023-06-17 17:02:13 +0700

    refactor map cache to texture cache

--- src/main/java/com/group/platformgame/levels/Level.java
+++ src/main/java/com/group/platformgame/levels/Level.java
@@ -2,8 +2,8 @@ package main.java.com.group.platformgame.levels;
 
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import main.java.com.group.platformgame.core.GamePanel;
 import main.java.com.group.platformgame.gameobjects.character.Player;
@@ -16,7 +16,7 @@ public class Level {
   public static int CELL_HEIGHT = 16;
   public static int CELL_WIDTH = 16;
 
-  private List<TextureMap> mapMem = new ArrayList<>();
+  private Map<Integer, BufferedImage> textureCache = new HashMap<>();
   private int[][] gridData;
   private Camera camera;
   private BufferedImage background;
@@ -31,7 +31,7 @@ public class Level {
     player = parser.getPlayer();
     background = Loader.loadBufferedImage("/resources/assets/images/platforms/Background.png");
   }
-  
+
   public BufferedImage getBackground() {
     return background;
   }
@@ -58,7 +58,6 @@ public class Level {
     int numRows = gridData.length - 1;
     int numCols = gridData[0].length - 1;
 
-
     int startX = Math.max(0, (camera.getX() - camera.getVisibleWidth()) / CELL_WIDTH);
     int startY = Math.max(0, (camera.getY() - camera.getVisibleHeight()) / CELL_HEIGHT);
 
@@ -68,14 +67,7 @@ public class Level {
     for (int row = startY; row < endY; row++) {
       for (int col = startX; col < endX; col++) {
         int textureKey = gridData[row][col];
-        BufferedImage textureImage = null;
-
-        for (TextureMap type : TextureMap.values()) {
-          if (type.inRange(textureKey)) {
-            if (!mapMem.contains(type)) mapMem.add(type);
-            textureImage = type.loadTexture(textureKey);
-          }
-        }
+        BufferedImage textureImage = getTextureImage(textureKey);
 
         int xPos = col * CELL_WIDTH;
         int yPos = row * CELL_HEIGHT;
@@ -83,7 +75,7 @@ public class Level {
         g2d.drawImage(textureImage, xPos, yPos, null);
       }
     }
-    
+
     player.render(g2d);
 
     g2d.setTransform(originalTransform);
@@ -91,5 +83,21 @@ public class Level {
 
   public Player getPlayer() {
     return player;
+  }
+
+  private BufferedImage getTextureImage(int textureKey) {
+    if (textureCache.containsKey(textureKey)) {
+      return textureCache.get(textureKey);
+    }
+
+    for (TextureMap type : TextureMap.values()) {
+      if (type.inRange(textureKey)) {
+        BufferedImage textureImage = type.loadTexture(textureKey);
+        textureCache.put(textureKey, textureImage);
+        return textureImage;
+      }
+    }
+
+    return null;
   }
 }
