a0c8741d78b2ac418256df05041afc3b0f955ca4
SinnerSS <ntnbusinf@gmail.com>
2023-06-29 01:14:45 +0700

    refactor to add hitbox

---
+++ src/main/java/com/group/platformgame/gameobjects/platform/Platform.java
@@ -0,0 +1,9 @@
+package main.java.com.group.platformgame.gameobjects.platform;
+
+import java.awt.Shape;
+
+import main.java.com.group.platformgame.gameobjects.GameObject;
+
+public class Platform {
+  
+}
