bc0c049dcca04b0bc478276c7545f3c4d679ba2c
SinnerSS <ntnbusinf@gmail.com>
2023-06-29 01:29:06 +0700

    basic platform implementation

--- src/main/java/com/group/platformgame/gameobjects/platform/Platform.java
+++ src/main/java/com/group/platformgame/gameobjects/platform/Platform.java
@@ -1,9 +1,26 @@
 package main.java.com.group.platformgame.gameobjects.platform;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Shape;
+import java.awt.image.BufferedImage;
 
 import main.java.com.group.platformgame.gameobjects.GameObject;
 
-public class Platform {
-  
+public class Platform<T extends Shape> extends GameObject<T> {
+  protected BufferedImage sprite;
+
+  public Platform(int x, int y, T hitbox, BufferedImage sprite) {
+    super(x, y, hitbox);
+    this.sprite = sprite;
+  }
+
+  @Override
+  public void update() {
+  }
+
+  @Override
+  public void render(Graphics2D g2D) {
+    g2D.drawImage(sprite, x, y, null);
+  }
 }
